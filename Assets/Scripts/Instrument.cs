24f56e697a2317a7edd5be252a6acf60882fecce
Drew Gingerich <gingerich93@gmail.com>
2019-01-27 03:04:54 -0800

    Add wrapper Instrument script as wrapper around audio source for more control.

---
+++ Assets/Scripts/Instrument.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrument : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+
+    float actualVolume;
+    bool volumeOverride;
+
+    bool FullVolume {
+        set {
+            actualVolume = audioSource.volume;
+            audioSource.volume = 1;
+            volumeOverride = true;
+        }    
+    }
+
+    bool Mute {
+        set { audioSource.mute = value; }
+    }
+
+    public void SetVolume(float volume) {
+        if (volumeOverride) {
+            return;
+        }
+        volume = Mathf.Clamp(volume, 0, 1);
+        audioSource.volume = volume;
+    }
+}
