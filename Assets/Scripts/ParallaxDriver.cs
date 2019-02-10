68ee0ac09e61db6c209a8fbf736b6c8a3bc8d5f9
Drew Gingerich <gingerich93@gmail.com>
2019-02-10 12:28:38 -0800

    Add script for parallaxing.

---
+++ Assets/Scripts/ParallaxDriver.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxDriver : MonoBehaviour
+{
+    [SerializeField]
+    Transform zReference;
+    [SerializeField]
+    bool horizontalParallax = true;
+    [SerializeField]
+    bool verticalParallax = false;
+    [SerializeField]
+    List<Transform> parallaxItems;
+
+    Vector3 previousPosition;
+
+    void Update()
+    {
+        float referenceZDiff = zReference.position.z - transform.position.z;
+        float inverseReferenceZDiff = 1 / referenceZDiff;
+        Vector3 referenceXYDiff = transform.position - previousPosition;
+
+        Vector3 parallaxShift;
+        foreach (Transform item in parallaxItems) {
+            float itemZDiff = zReference.position.z - item.position.z;
+            float moveRatio = 1 - itemZDiff * inverseReferenceZDiff;
+            parallaxShift = referenceXYDiff * moveRatio;
+
+            parallaxShift.z = 0;
+            if (!horizontalParallax) {
+                parallaxShift.x = 0;
+            }
+            if (!verticalParallax) {
+                parallaxShift.y = 0;
+            }
+
+            item.position += parallaxShift;
+        }
+
+        previousPosition = transform.position;
+    }
+
+
+}
