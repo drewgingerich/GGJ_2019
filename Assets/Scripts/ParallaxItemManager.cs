16aa885975a3d4d3e56ba2766b1477341aafe9cb
Drew Gingerich <gingerich93@gmail.com>
2019-02-16 14:08:08 -0800

    Add script to more easily manage adding parallax items.

---
+++ Assets/Scripts/ParallaxItemManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxItemManager : MonoBehaviour
+{
+    [SerializeField]
+    ParallaxDriver parallaxDriver;
+    [SerializeField]
+    List<Transform> parallaxItems;
+
+    void Start() {
+        foreach (Transform item in parallaxItems) {
+            parallaxDriver.AddParallaxItem(item);
+        }
+    }
+}
