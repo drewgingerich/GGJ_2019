b927f79ab0b8ba386628946591c2c5370c9c2a08
nextlevelbanana <qristyoverton@gmail.com>
2019-02-18 11:05:25 -0800

    wip

---
+++ Assets/Scripts/ScreenTransition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenTransition : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private bool done;
+
+    private float progress;
+
+    [SerializeField]
+    private Image image;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Transition() {
+        Debug.Log("transitioning!");
+        done = false;
+        progress = 0f;
+        while (progress < 1 ) {
+            progress += Time.deltaTime * (1f /speed);
+            image.fillAmount = progress;
+            yield return null;
+        }
+        done = true;
+    }
+}
