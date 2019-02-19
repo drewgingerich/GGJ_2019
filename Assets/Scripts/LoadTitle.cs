62aeb65cd920782c6d8a4a69da0c760210a58f14
nextlevelbanana <qristyoverton@gmail.com>
2019-02-18 19:51:17 -0800

    bumper/title/credits enhancements

--- Assets/Scripts/LoadTitle.cs
+++ Assets/Scripts/LoadTitle.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class LoadTitle : MonoBehaviour
 {
 
+    public Image teamTitleCard;
     public VideoPlayer video;
 
+    void Start() {
+        teamTitleCard.fillAmount = 0;
+    }
     void Awake() {
         video = GetComponent<VideoPlayer>();
         video.loopPointReached += OnMovieFinished; 
@@ -17,7 +22,15 @@ public class LoadTitle : MonoBehaviour
 
     void OnMovieFinished(VideoPlayer player)
     {
-        Debug.Log("Event for movie end called");
+        teamTitleCard.fillAmount = 1;
+        StartCoroutine(WaitThenSceneChangeRoutine());
+        
+    }
+
+    IEnumerator WaitThenSceneChangeRoutine() {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("scene change");
         SceneManager.LoadScene("Title");
+        yield return null;
     }
 }
