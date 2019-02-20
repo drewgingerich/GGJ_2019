ff032cb0b34f2a607dfd0939bf8c914981491ffe
Drew Gingerich <gingerich93@gmail.com>
2019-02-20 15:45:38 -0800

    Add script to handle interactable things.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public static List<Interactable> interactables;
+
+    public UnityEvent OnInteract;
+    public UnityEvent OnSelect;
+    public UnityEvent OnDeselect;
+
+    public void Interact() {
+        OnInteract.Invoke();
+    }
+
+    public void Select() {
+        OnSelect.Invoke();
+    }
+
+    public void Deselect() {
+        OnDeselect.Invoke();
+    }
+
+    void OnEnable() {
+        if (interactables == null) {
+            interactables = new List<Interactable>();
+        }
+        interactables.Add(this);
+    }
+
+    void OnDisable() {
+        interactables.Remove(this);
+    }
+}
