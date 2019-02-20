36ac914c44de636cf68316d9bf87c40f4a74acd7
Drew Gingerich <gingerich93@gmail.com>
2019-02-20 15:45:15 -0800

    Add script to control particle system spawn color.

---
+++ Assets/Scripts/ParticleColorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleColorController : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem.MinMaxGradient color;
+    [SerializeField]
+    ParticleSystem.MinMaxGradient selectedColor;
+    [SerializeField]
+    ParticleSystem.MinMaxGradient interactingColor;
+    [SerializeField]
+    new ParticleSystem particleSystem;
+    
+    ParticleSystem.MainModule mainModule;
+
+    void Start()
+    {
+        mainModule = particleSystem.main;
+        mainModule.startColor = color;
+    }
+
+    public void Select() {
+        mainModule.startColor = selectedColor;
+    }
+
+    public void Deselect() {
+        mainModule.startColor = color;
+    }
+
+    public void Interact() {
+        mainModule.startColor = interactingColor;
+    }
+}
